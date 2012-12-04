1a30732e8ce04e38606011c5886eec8a18357bf5
jezzsa <jezz.santos@hotmail.com>
2012-12-04 14:24:31 +1300

    Added correct imageconverter for commandbar icons

---
+++ Src/Common/Source/Common.Presentation/Data/DisabledImageConverter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.VisualStudio.PlatformUI;
+
+namespace Microsoft.VisualStudio.Patterning.Common.Presentation.Data
+{
+#if VSVER10
+	/// <summary>
+	/// Converts an image <see cref="BitmapSource"/> into a disabled version of the same image as an <see cref="Image"/>.
+	/// </summary>
+	[ValueConversion(typeof(BitmapImage), typeof(Image))]
+	public sealed class DisabledImageConverter : PlatformUI.GrayscaleImageConverter
+    {	}
+#endif
+#if VSVER11
+    /// <summary>
+    /// Converts an image <see cref="BitmapSource"/> into a disabled version of the same image as an <see cref="Image"/>.
+	/// </summary>
+	[ValueConversion(typeof(BitmapImage), typeof(Image))]
+    public sealed class DisabledImageConverter : ValueConverter<BitmapSource, Image>
+    {
+        private ThemedImageConverter innerConverter;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="DisabledImageConverter"/> class.
+        /// </summary>
+        public DisabledImageConverter()
+        {
+            this.innerConverter = new ThemedImageConverter();
+        }
+
+        /// <summary>
+        /// Converts the image to a disabled image
+        /// </summary>
+        /// <remarks>This is based on <see cref="ThemedImageConverter"/></remarks>
+        protected override Image Convert(BitmapSource value, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var backgroundColor = Colors.Transparent; // Background color of all commandbar images
+
+            if (parameter == null)
+            {
+                parameter = (Color) ColorConverter.ConvertFromString("#40FFFFFF");
+            }
+
+            if (value == null)
+            {
+                return null;
+            }
+            BitmapSource source;
+            Color grayscaleBiasColor = Colors.White;
+            if (parameter is Color)
+            {
+                grayscaleBiasColor = (Color)parameter;
+            }
+            source = ImageThemingUtilities.GetOrCreateThemedBitmapSource(value, backgroundColor, false, grayscaleBiasColor);
+
+            Image image = new Image();
+            image.BeginInit();
+            image.Source = source;
+            image.EndInit();
+            return image;
+        }
+
+        /// <summary>
+        /// Reverts the converted image
+        /// </summary>
+        protected override BitmapSource ConvertBack(Image value, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+#endif
+}
