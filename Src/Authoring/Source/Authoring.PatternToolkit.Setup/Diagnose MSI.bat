5cf706c7291859abdd191b88f02deb824ec7bd84
jezzsa <jezz.santos@hotmail.com>
2013-02-09 12:15:02 +1300

    Added setup solution

--- Src/Authoring/Source/Authoring.PatternToolkit.Setup/Diagnose MSI.bat
+++
@@ -1,5 +0,0 @@
-REM More verbose logging
-REM msiexec.exe /i "Authoring.PatternToolkit.Setup.msi" /l*v "Authoring.PatternToolkit.Setup.log"
-
-REM Basic logging
-msiexec.exe /i "Authoring.PatternToolkit.Setup.msi" /log "Authoring.PatternToolkit.Setup.log"
