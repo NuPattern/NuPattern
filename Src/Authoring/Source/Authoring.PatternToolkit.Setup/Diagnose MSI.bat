67301400093417bff62ffff05920884b93c5b3b6
jezzsa <jezz.santos@hotmail.com>
2013-02-09 01:26:51 +1300

    Updated launch conditions, and warnings

---
+++ Src/Authoring/Source/Authoring.PatternToolkit.Setup/Diagnose MSI.bat
@@ -0,0 +1,5 @@
+REM More verbose logging
+REM msiexec.exe /i "Authoring.PatternToolkit.Setup.msi" /l*v "Authoring.PatternToolkit.Setup.log"
+
+REM Basic logging
+msiexec.exe /i "Authoring.PatternToolkit.Setup.msi" /log "Authoring.PatternToolkit.Setup.log"
