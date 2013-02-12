87baf3b3e42eb98a7230b6be4add2575777d7523
jezzsa <jezz.santos@hotmail.com>
2013-02-12 15:29:05 +1300

    Fixed up integration tests

--- Src/Common/Helpers/IntegrationTest.cs
+++ Src/Common/Helpers/IntegrationTest.cs
@@ -8,65 +8,70 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 /// <summary>
 /// Base class for integration tests that need to deploy content.
 /// </summary>
+/// <remarks>
+/// This class works around the issue of resetting the test data per test that VS has.
+/// VS currently only copies test data once per test run, and if a test alters that test data then the test is not correctly reset for other tests.
+/// This class copies the test data every test, and ensures that the test datat is always reset between tests.
+/// </remarks>
 public class IntegrationTest
 {
-	private TestContext context;
+    private TestContext context;
 
-	/// <summary>
-	/// Gets or sets the location of the per-test location directory.
-	/// </summary>
-	public string DeploymentDirectory { get; set; }
+    /// <summary>
+    /// Gets or sets the location of the per-test location directory.
+    /// </summary>
+    public string DeploymentDirectory { get; set; }
 
-	/// <summary>
-	/// Gets or sets the test context.
-	/// </summary>
-	public TestContext TestContext
-	{
-		get
-		{
-			return this.context;
-		}
+    /// <summary>
+    /// Gets or sets the test context.
+    /// </summary>
+    public TestContext TestContext
+    {
+        get
+        {
+            return this.context;
+        }
 
-		set
-		{
-			this.context = value;
-			this.DeploymentDirectory = Path.Combine(Path.GetTempPath(), this.TestDirectory);
-			this.CleanDeploymentDirectory();
+        set
+        {
+            this.context = value;
+            this.DeploymentDirectory = Path.Combine(Path.GetTempPath(), this.TestDirectory);
+            this.CleanDeploymentDirectory();
 
-			// On set, we copy a branch of the deployment items
-			var deploymentItems = this.GetType()
-				.GetCustomAttributes(typeof(DeploymentItemAttribute), true)
-				.OfType<DeploymentItemAttribute>();
+            // On set, we copy a branch of the deployment items
+            var deploymentItems = this.GetType()
+                .GetCustomAttributes(typeof(DeploymentItemAttribute), true)
+                .OfType<DeploymentItemAttribute>();
 
-			foreach (var deploymentItem in deploymentItems)
-			{
-				var sourceDir = string.IsNullOrEmpty(deploymentItem.OutputDirectory) ?
-					this.context.DeploymentDirectory :
-					Path.Combine(this.context.DeploymentDirectory, deploymentItem.OutputDirectory);
+            foreach (var deploymentItem in deploymentItems)
+            {
+                var sourceDir = string.IsNullOrEmpty(deploymentItem.OutputDirectory) ?
+                    this.context.DeploymentDirectory :
+                    Path.Combine(this.context.DeploymentDirectory, deploymentItem.OutputDirectory);
 
-				// Fix VSTS non-copy-if-folder exists "issue"
-				RunXCopy(sourceDir, Path.Combine(this.DeploymentDirectory, deploymentItem.OutputDirectory));
-			}
-		}
-	}
+                // Fix VSTS non-copy-if-folder exists "issue"
+                RunXCopy(sourceDir, Path.Combine(this.DeploymentDirectory, deploymentItem.OutputDirectory));
+            }
+        }
+    }
 
-	private string TestDirectory
-	{
-		get { return ((uint)this.context.TestName.GetHashCode()).ToString(); }
-	}
+    private string TestDirectory
+    {
+        get { return ((uint)this.context.TestName.GetHashCode()).ToString(); }
+    }
 
-	/// <summary>
-	/// Returns a path relative to the test deployment directory.
-	/// </summary>
-	/// <param name="deploymentRelativePath">A relative path that is resolved from the deployment directory.</param>
-	/// <returns>The path relative to the test deployment directory.</returns>
-	protected string PathTo(string deploymentRelativePath)
-	{
-		return Path.Combine(this.DeploymentDirectory, deploymentRelativePath);
-	}
+    /// <summary>
+    /// Returns a path relative to the test deployment directory.
+    /// </summary>
+    /// <param name="deploymentRelativePath">A relative path that is resolved from the deployment directory.</param>
+    /// <returns>The path relative to the test deployment directory.</returns>
+    protected string PathTo(string deploymentRelativePath)
+    {
+        return Path.Combine(this.DeploymentDirectory, deploymentRelativePath);
+    }
 
-	private static void RunXCopy(string sourceDir, string targetDir)
-	{
+    private static void RunXCopy(string sourceDir, string targetDir)
+    {
         try
         {
             var startInfo = new ProcessStartInfo(
@@ -85,17 +90,17 @@ public class IntegrationTest
             throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture,
                 "Failed to xcopy integration test dependencies from '{0}' to '{1}'.", sourceDir, targetDir), ex);
         }
-	}
+    }
 
-	private void CleanDeploymentDirectory()
-	{
-		if (Directory.Exists(this.DeploymentDirectory))
-		{
-			// Clear readonly attributes
-			foreach (var file in Directory.EnumerateFiles(this.DeploymentDirectory, "*.*", SearchOption.AllDirectories))
-			{
-				File.SetAttributes(file, FileAttributes.Normal);
-			}
+    private void CleanDeploymentDirectory()
+    {
+        if (Directory.Exists(this.DeploymentDirectory))
+        {
+            // Clear readonly attributes
+            foreach (var file in Directory.EnumerateFiles(this.DeploymentDirectory, "*.*", SearchOption.AllDirectories))
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+            }
 
             try
             {
@@ -110,6 +115,6 @@ public class IntegrationTest
             {
                 // Ignore and continue
             }
-		}
-	}
+        }
+    }
 }
