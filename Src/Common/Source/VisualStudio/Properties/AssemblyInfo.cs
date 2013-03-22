ac939f025fd7fcbf9835f6e7319221f25b4ff71b
jezzsa <jezz.santos@hotmail.com>
2013-03-22 17:12:07 +1300

    Added new VisualStudio project. Added references to new project Updated solution configurations

---
+++ Src/Common/Source/VisualStudio/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.ExtensibilityHosting;
+using Microsoft.VisualStudio.TeamArchitect.PowerTools;
+
+[assembly: AssemblyTitle("NuPattern.VisualStudio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: VsCatalogName(Constants.CatalogName)]
+[assembly: VsCatalogName("Microsoft.VisualStudio.Default")]
