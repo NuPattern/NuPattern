d8cf21d8ccc08f0951adc4f6b8aee31b3c97a833
jezzsa <jezz.santos@hotmail.com>
2013-04-12 11:48:54 +1200

    Fixed bug in command validation attributes

--- Src/Library/Source/Automation/Command/Validation/ICommandValidationRuleMetadata.cs
+++ Src/Library/Source/Automation/Command/Validation/ICommandValidationRuleMetadata.cs
@@ -2,14 +2,14 @@
 
 namespace NuPattern.Library
 {
-	/// <summary>
-	/// Metadata for the  <see cref="ICommandValidationRule"/>
-	/// </summary>
-    internal interface ICommandValidationRuleMetadata
-	{
-		/// <summary>
-		/// The type of the command to which the validations apply
-		/// </summary>
-		Type CommandType { get; }
-	}
+    /// <summary>
+    /// Metadata for the  <see cref="ICommandValidationRule"/>
+    /// </summary>
+    public interface ICommandValidationRuleMetadata
+    {
+        /// <summary>
+        /// The type of the command to which the validations apply
+        /// </summary>
+        Type CommandType { get; }
+    }
 }
