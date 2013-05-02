d34b3e26d14165b82554de252abf4d93846a1a8c
Daniel Cazzulino <daniel@cazzulino.com>
2013-05-02 12:16:41 -0300

    Fixed missing GetSource member on the TracerManager implementation.

--- Src/Common/Source/Common/Diagnostics/Customizations.cs
+++ Src/Common/Source/Common/Diagnostics/Customizations.cs
@@ -34,6 +34,17 @@
         }
     }
 
+    partial class TracerManager
+    {
+        /// <summary>
+        /// Gets the underlying <see cref="TraceSource"/> for the given name.
+        /// </summary>
+        public TraceSource GetSource(string name)
+        {
+            return GetOrAdd(name, s => CreateSource(s));
+        }
+    }
+
     partial interface ITracerManager
     {
         /// <summary>
