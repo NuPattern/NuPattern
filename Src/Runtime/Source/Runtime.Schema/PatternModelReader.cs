a157d9ecda725dd2f5bc11022a59791741abc81f
Daniel Cazzulino <daniel@cazzulino.com>
2013-05-03 01:17:31 -0300

    Fixes issue 103 by enabling the use of the directive processors from outside the authoring toolkit.
    Adds customization to the codegen for directive processors, which is shared by both processors we have.
    
    Signed-off-by: jezzsa <jezz.santos@hotmail.com>

---
+++ Src/Runtime/Source/Runtime.Schema/PatternModelReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.Modeling;
+
+namespace NuPattern.Runtime.Schema
+{
+    /// <summary>
+    /// Reads a serialized product state model in memory.
+    /// </summary>
+    public static class PatternModelReader
+    {
+        /// <summary>
+        /// Root type of the domain model, for modeling transformation initialization purposes.
+        /// </summary>
+        public static Type DomainModelType
+        {
+            get { return typeof(PatternModelDomainModel); }
+        }
+
+        /// <summary>
+        /// Reads the model into the given store and returns it.
+        /// </summary>
+        public static IPatternModelSchema Read(SerializationResult result, Store store, string modelFile)
+        {
+            return PatternModelSerializationHelper.Instance.LoadModel(result, store, modelFile, null, null, null);
+        }
+    }
+}
---
+++ Src/Runtime/Source/Runtime.Store/ProductStateModelReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Dsl = Microsoft.VisualStudio.Modeling;
+
+namespace NuPattern.Runtime.Store
+{
+    /// <summary>
+    /// Reads a serialized product state model in memory.
+    /// </summary>
+    public static class ProductStateModelReader
+    {
+        /// <summary>
+        /// Root type of the domain model, for modeling transformation initialization purposes.
+        /// </summary>
+        public static Type DomainModelType
+        {
+            get { return typeof(ProductStateStoreDomainModel); }
+        }
+
+        /// <summary>
+        /// Reads the model into the given store and returns it.
+        /// </summary>
+        public static IProductState Read(Dsl.SerializationResult result, Dsl.Store store, string modelFile)
+        {
+            return ProductStateStoreSerializationHelper.Instance.LoadModel(result, store, modelFile, null, null, null);
+        }
+    }
+}
