90599917e23895f8a87a169ee8064c98537f5f13
Daniel Cazzulino <daniel@cazzulino.com>
2013-05-05 10:51:16 -0300

    Renamed DropSolutionItemConditionSpec.cs to DragDropHelpersSpec.cs since the tests aren't about the condition but the helpers' extension methods.

---
+++ Src/Library/UnitTests/Conditions/DragDropHelpersSpec.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuPattern.Library.Conditions;
+
+namespace NuPattern.Library.UnitTests.Conditions
+{
+    [TestClass]
+    public class DragDropHelpersSpec
+    {
+        internal static readonly IAssertion Assert = new Assertion();
+
+        [TestClass]
+        public class GivenNoContext
+        {
+            private DropSolutionItemCondition condition;
+
+            [TestInitialize]
+            public void InitializeContext()
+            {
+                this.condition = new DropSolutionItemCondition();
+            }
+
+            [TestMethod, TestCategory("Unit")]
+            public void WhenSingleExtensionDoesNotMatch_ThenGetPathsEndingWithExtensionsReturnsNoPaths()
+            {
+                var extensions = "cs";
+                var paths = new List<string>(new[] { "Foo.xml" });
+                var results = paths.GetPathsEndingWithExtensions(extensions);
+
+                Assert.False(results.Any());
+            }
+
+            [TestMethod, TestCategory("Unit")]
+            public void WhenSingleExtensionWithNoPaths_ThenGetPathsEndingWithExtensionsReturnsNoPaths()
+            {
+                var extensions = "cs";
+                var paths = new List<string>();
+                var results = paths.GetPathsEndingWithExtensions(extensions);
+
+                Assert.False(results.Any());
+            }
+
+            [TestMethod, TestCategory("Unit")]
+            public void WhenSingleExtensionContainsNoPeriod_ThenGetPathsEndingWithExtensionsReturnsPaths()
+            {
+                var extensions = "cs";
+                var paths = new List<string>(new[] { "Foo.cs", "Bar.cs", "Foo.xml" });
+                var results = paths.GetPathsEndingWithExtensions(extensions);
+
+                Assert.True(results.Match(new[] { "Foo.cs", "Bar.cs" }));
+            }
+
+            [TestMethod, TestCategory("Unit")]
+            public void WhenSingleExtensionContainsOnlyPeriod_ThenGetPathsEndingWithExtensionsReturnsPaths()
+            {
+                var extensions = ".cs";
+                var paths = new List<string>(new[] { "Foo.cs", "Bar.cs", "Foo.xml" });
+                var results = paths.GetPathsEndingWithExtensions(extensions);
+
+                Assert.True(results.Match(new[] { "Foo.cs", "Bar.cs" }));
+            }
+
+            [TestMethod, TestCategory("Unit")]
+            public void WhenSingleExtensionContainsWildcardAndPeriod_ThenGetPathsEndingWithExtensionsReturnsPaths()
+            {
+                var extensions = "*.cs";
+                var paths = new List<string>(new[] { "Foo.cs", "Bar.cs", "Foo.xml" });
+                var results = paths.GetPathsEndingWithExtensions(extensions);
+
+                Assert.True(results.Match(new[] { "Foo.cs", "Bar.cs" }));
+            }
+
+            [TestMethod, TestCategory("Unit")]
+            public void WhenMultiExtensionContainsNoPeriod_ThenGetPathsEndingWithExtensionsReturnsPaths()
+            {
+                var extensions = "cs;xml;tt";
+                var paths = new List<string>(new[] { "Foo.cs", "Bar.cs", "Foo.xml", "Bar.tt" });
+                var results = paths.GetPathsEndingWithExtensions(extensions);
+
+                Assert.True(results.Match(new[] { "Foo.cs", "Bar.cs", "Foo.xml", "Bar.tt" }));
+            }
+
+            [TestMethod, TestCategory("Unit")]
+            public void WhenMultiExtensionContainsOnlyPeriod_ThenGetPathsEndingWithExtensionsReturnsPaths()
+            {
+                var extensions = ".cs;.xml;.tt";
+                var paths = new List<string>(new[] { "Foo.cs", "Bar.cs", "Foo.xml", "Bar.tt", "Foo.xaml" });
+                var results = paths.GetPathsEndingWithExtensions(extensions);
+
+                Assert.True(results.Match(new[] { "Foo.cs", "Bar.cs", "Foo.xml", "Bar.tt" }));
+            }
+
+            [TestMethod, TestCategory("Unit")]
+            public void WhenMultiExtensionContainsWildcardAndPeriod_ThenGetPathsEndingWithExtensionsReturnsPaths()
+            {
+                var extensions = "*.cs;*.xml;*.tt";
+                var paths = new List<string>(new[] { "Foo.cs", "Bar.cs", "Foo.xml", "Bar.tt", "Foo.xaml" });
+                var results = paths.GetPathsEndingWithExtensions(extensions);
+
+                Assert.True(results.Match(new[] { "Foo.cs", "Bar.cs", "Foo.xml", "Bar.tt" }));
+            }
+
+            [TestMethod, TestCategory("Unit")]
+            public void WhenMultiExtensionContainsMixture_ThenGetPathsEndingWithExtensionsReturnsPaths()
+            {
+                var extensions = "*.cs;.xml;tt";
+                var paths = new List<string>(new[] { "Foo.cs", "Bar.cs", "Foo.xml", "Bar.tt", "Foo.xaml" });
+                var results = paths.GetPathsEndingWithExtensions(extensions);
+
+                Assert.True(results.Match(new[] { "Foo.cs", "Bar.cs", "Foo.xml", "Bar.tt" }));
+            }
+        }
+    }
+
+    internal static class SetExtensions
+    {
+        public static bool Match(this IEnumerable<string> a, IEnumerable<string> b)
+        {
+            return a.Intersect<string>(b).Any()
+                && !a.Except<string>(b).Any()
+                && b.Intersect<string>(a).Any()
+                && !b.Except<string>(a).Any();
+        }
+    }
+}
