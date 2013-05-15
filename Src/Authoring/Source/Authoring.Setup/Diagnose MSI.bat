0b9b2d916f01afacb4b1b98529fac105e387afb0
jezzsa <jezz.santos@hotmail.com>
2013-05-16 01:30:39 +1200

    fixed up installer for upgrades

--- Src/Authoring/Source/Authoring.Setup/Diagnose MSI.bat
+++ Src/Authoring/Source/Authoring.Setup/Diagnose MSI.bat
@@ -1,5 +1,5 @@
 REM More verbose logging
-REM msiexec.exe /i "Authoring.Setup.msi" /l*v "Authoring.Setup.log"
+REM msiexec.exe /i "NuPatternToolkitBuilderAndLabs.msi" /l*v "NuPatternToolkitBuilderAndLabs.log"
 
 REM Basic logging
-msiexec.exe /i "Authoring.Setup.msi" /log "Authoring.Setup.log"
+msiexec.exe /i "NuPatternToolkitBuilderAndLabs.msi" /log "NuPatternToolkitBuilderAndLabs.log"
