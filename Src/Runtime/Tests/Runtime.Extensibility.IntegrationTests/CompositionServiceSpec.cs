292e21d93bde08c8d008ff3cc260a59f7656df44
Daniel Cazzulino <daniel@cazzulino.com>
2014-01-23 16:49:39 -0300

    Added new test for composition stuff.

---
+++ Src/Runtime/Tests/Runtime.Extensibility.IntegrationTests/CompositionServiceSpec.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VSSDK.Tools.VsIdeTesting;
+using NuPattern.Runtime.Composition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuPattern.Runtime.IntegrationTests
+{
+    [TestClass]
+    public class CompositionServiceSpec
+    {
+        [HostType("VS IDE")]
+        [TestMethod]
+        public void When_Retieving_Composition_Service_Then_Succeeds()
+        {
+            var components = VsIdeTestHostContext.ServiceProvider.GetService<SComponentModel, IComponentModel>();
+
+            var composition = components.GetService<INuPatternCompositionService>();
+
+            Assert.IsNotNull(composition);
+        }
+    }
+}
